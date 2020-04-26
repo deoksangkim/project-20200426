e9ad00a2e421f84f6583306c28154f7dd9cbb318
김범 <kqw1209@gmail.com>
2020-04-26 14:26:32 +0900

    initial commit

---
+++ src/main/java/com/projet/template/project/entities/MemberEntity.java
@@ -0,0 +1,16 @@
+package com.projet.template.project.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class MemberEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String name;
+}
---
+++ src/test/java/com/projet/template/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.template.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
